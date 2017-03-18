59fd3997d4e8bf8a1d18b25f30e3073eac13a70c
Or Israeli <tur103103@gmail.com>
2017-03-18 17:52:28 +0200

    initial server

---
+++ Desktop/server/constants.py
@@ -0,0 +1,23 @@
+HOST = "0.0.0.0"
+PORT = 8820
+NUMBER_OF_CLIENTS = 1
+BUFFER = 1024
+IMAGE_BUFFER = 300000
+TEXT_BUFFER = 100
+
+OK = "OK"
+NO = "NO"
+
+DATABASE = "database"
+LOG_IN = "login"
+REGISTER = "register"
+CHANGE = "change"
+CONTACT = "contact"
+GET_FRAMES = "getframes"
+UPLOAD_PICTURE = "uploadpicture"
+UPLOAD_STATUS = "uploadstatus"
+
+DIRECTORY = "C:/work/facebookframes/"
+
+MY_EMAIL = "tur103103@gmail.com"
+MY_PASSWORD = "103103103"
