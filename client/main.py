f1bf9cfa51d6a33a4e95951e848b9446d68096e4
Or Israeli <tur103103@gmail.com>
2017-03-24 21:43:22 +0300

    add thread

--- client/main.py
+++ client/main.py
@@ -1,16 +1,9 @@
-from auth_page import *
-from constants import *
-
-
-root = Tk()
+from app import *
 
 
 def main():
-    auth_page = AuthPage(root)
-    menubutton_auth = auth_page.set_menu_button(root)
-    menubutton_auth.pack()
-    menubutton_auth.place(x=LEFTT)
-    root.mainloop()
+    app = App()
+
 
 
 if __name__ == '__main__':
